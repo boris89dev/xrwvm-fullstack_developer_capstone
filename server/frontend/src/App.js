5e6c2fe3878d2cedad6b431c7d515a5e7c4a127c
boris89dev <carollobor@gmail.com>
2025-04-07 19:12:12 -0400

    Get dealers

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import LoginPanel from "./components/Login/Login";
 import Register from "./components/Register/Register";
+import Dealers from './components/Dealers/Dealers';
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
-      <Route path="/register" element={<Register />} /> {/* Aggiungi la route per la registrazione */}
+      <Route path="/register" element={<Register />} /> {}
+      <Route path="/dealers" element={<Dealers/>} />
     </Routes>
   );
 }
