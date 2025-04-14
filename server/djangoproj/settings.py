701fa67e6afc6ff9e5d91e9160e16f0084c44d85
boris89dev <carollobor@gmail.com>
2025-04-14 04:15:58 -0400

    Create main.yml

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
-    "https://carollobor-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01." \
+    "https://carollobor-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01."
     "proxy.cognitiveclass.ai",
 ]
 
